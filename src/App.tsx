c4098deb78c861fa2523f811d6c6031140b1541d
bwapes <benwaples@gmail.com>
2020-10-28 13:44:42 -0700

    connect header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import { CharacterList } from './components/CharacterList/CharacterList';
+import { Header } from './components/Header/Header';
 
 function App() {
   return (
    <>
+   <Header />
    <CharacterList />
    </>
   );
